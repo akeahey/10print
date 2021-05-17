0d7deb853e83f48c400919808e5bd86290f119c2
akeahey <40642108+akeahey@users.noreply.github.com>
2021-05-17 00:04:05 -0500

    finished

--- 10print.js
+++ 10print.js
@@ -1,20 +1,25 @@
 /*
-  one possible implemenation of the 10print.org algorithm written in JavaScript
+  I've been spending a lot of time looking at water recently, and the random (but not?)
+  patterns in how it moves, rushes, ebbs, and flows. I wanted to create something
+  that would remind me of those patterns and how the sun shines off of them
 */
 
 const w = process.stdout.columns
+const arr = ['\x1b[44m\x1b[37m', '\x1b[44m\x1b[36m', '\x1b[44m\x1b[32m', '\x1b[44m\x1b[33m']
+const lines = ['╱', '╲', '╳']
 
 function draw () {
-  setTimeout(draw, 1000)
+  setTimeout(draw, 1000/30)
   let output = ''
   for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
-    }
+    let x = Math.random()
+    const r = Math.floor(Math.random() * arr.length);
+    output += arr[r]
+    const s = Math.floor(Math.random() * lines.length);
+    output += lines[s]
   }
   console.log(output)
 }
 
 draw()
+
